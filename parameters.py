988d3e9c6aad7e44db90130604faab3922ec1d9f
Olga Kovaleva <okovaleva@shala.cs.uml.edu>
2018-11-11 16:58:16 -0500

    parameters added

---
+++ parameters.py
@@ -0,0 +1,17 @@
+params = {
+    'max_len': 15,
+    'hidden_size': 256,
+    'embedding_dim': 300,
+    'batch_size': 300,
+    'n_epochs': 1000,
+    'N': 5
+}
+
+locations = {
+    'train_loc': 'data/train',
+    'dev_loc': 'data/dev',
+    'fasttext_loc': '/data1/word_vectors/fastText/crawl-300d-2M.vec',
+    'w2vec_loc': 'vocabulary/word_to_vec.pkl',
+    'model_loc': 'models/model.pt',
+    'stopwordsfile': 'data/stop_words.txt'
+}
